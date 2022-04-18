84f82a5840dda28504b380a389618f864dc16584
chrishughallen <chrishughallen@gmail.com>
2022-04-18 15:31:13 +0200

    fixed fill of mobile toggle

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -21,7 +21,7 @@ export default function Banner() {
         <h1>Chateau de Villeconin</h1>
         <p>Un château médiéval en Essonne</p>
       </NavLink>
-      <svg onClick={toggleMobileMenu} fill="#fff" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 24 24" width="24px" height="24px"><path d="M 2 5 L 2 7 L 22 7 L 22 5 L 2 5 z M 2 11 L 2 13 L 22 13 L 22 11 L 2 11 z M 2 17 L 2 19 L 22 19 L 22 17 L 2 17 z"/></svg>
+      <svg onClick={toggleMobileMenu} fill="000" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 24 24" width="24px" height="24px"><path d="M 2 5 L 2 7 L 22 7 L 22 5 L 2 5 z M 2 11 L 2 13 L 22 13 L 22 11 L 2 11 z M 2 17 L 2 19 L 22 19 L 22 17 L 2 17 z"/></svg>
     </div>
     </>
   )
