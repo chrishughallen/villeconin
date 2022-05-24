68509e20696b35384b98cd22a8b57a307eb74b37
chrishughallen <chrishughallen@gmail.com>
2022-05-24 11:52:55 +0200

    add scroll to top

--- src/App.js
+++ src/App.js
@@ -4,19 +4,22 @@ import Header from './Components/Header.js';
 import Home from './Pages/Home.js';
 import About from './Pages/About.js';
 import Contact from './Pages/Contact.js';
+import ScrollToTop from "./ScrollToTop";
 import './App.css';
 
 function App() {
   return (
     <Router>
+      <ScrollToTop>
        <Header />
-      <div className="wrapper">
-        <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/about" element={<About />}></Route>
-          <Route path="/contact" element={<Contact />}></Route>
-        </Routes>
-      </div>
+        <div className="wrapper">
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/about" element={<About />}></Route>
+            <Route path="/contact" element={<Contact />}></Route>
+          </Routes>
+        </div>
+      </ScrollToTop>
     </Router>
   );
 }
